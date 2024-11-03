6f1dd770315bd92afb8da529a84a36f60a7864ef
John Jorel Landicho <152581325+jtlandicho@users.noreply.github.com>
2024-11-03 08:09:53 +0000

    adding login csssssss

--- ImPossibleSystem/ImPossibleSystem/urls.py
+++ ImPossibleSystem/ImPossibleSystem/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path ('', views.SignUpPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home')
 ]
