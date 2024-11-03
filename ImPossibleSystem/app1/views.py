a63f21b4fc73edbcd53fe19b544dff8f407b5fbf
John Jorel Landicho <152581325+jtlandicho@users.noreply.github.com>
2024-11-03 08:40:48 +0000

    trying signup page

--- ImPossibleSystem/app1/views.py
+++ ImPossibleSystem/app1/views.py
@@ -6,6 +6,14 @@ def HomePage(request):
     return render (request,'home.html')
 
 def SignUpPage(request):
+    if request.method=='POST':
+        uname=request.POST.get("firstName")
+        lname=request.POST.get("lastName")
+        email=request.POST.get("email")
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('passsword2')
+
+        print(uname,lname,email,pass1,pass2)
     return render (request, 'signup.html')
 
 def LoginPage(request):
